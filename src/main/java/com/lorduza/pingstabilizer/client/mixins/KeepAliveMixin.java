23f5b5e7af1104a367d94cb99930761aa853d1d6
lorduza <lorduza@users.noreply.github.com>
2025-12-12 00:07:16 +0300

    feat: Add keep-alive packet tracking mixin for ping stabilization and include mod icon.

--- src/main/java/com/lorduza/pingstabilizer/client/mixins/KeepAliveMixin.java
+++ src/main/java/com/lorduza/pingstabilizer/client/mixins/KeepAliveMixin.java
@@ -2,14 +2,14 @@ package com.lorduza.pingstabilizer.client.mixins;
 
 import com.lorduza.pingstabilizer.lib.LatencySensor;
 import com.lorduza.pingstabilizer.lib.PingTracker;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientCommonNetworkHandler;
 import net.minecraft.network.packet.s2c.common.KeepAliveS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayNetworkHandler.class)
+@Mixin(ClientCommonNetworkHandler.class)
 public class KeepAliveMixin {
     
     @Inject(method = "onKeepAlive", at = @At("HEAD"))
